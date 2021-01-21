94a5fa5f3a0908c71d439dfe0515bfee1b7e766a
Mr_Lee <49835246+barahana20@users.noreply.github.com>
2021-01-21 23:28:02 +0900

    difficult very difficult

--- 그냥 심심할 떄 코딩하는 곳/간단하게 만드는 게임.py
+++ 그냥 심심할 떄 코딩하는 곳/간단하게 만드는 게임.py
@@ -3,16 +3,40 @@ import os
 import sys
 import pygame
 
-pygame.init()
+class Weapon:
+    pass
 
-screen_width = 480 # 가로 크기
-screen_height = 640 # 세로 크기
-pygame.display.set_mode((screen_width, screen_height))
+class Sword(Weapon):
+    def __init__(self, money, damage):
+        self.money = money
+        self.damage = damage
 
-running = True
-while running:
-    for event in pygame.event.get(): # 어떤 이벤트가 발생하였는가?
-         if event.type == pygame.QUIT: # 창이 닫히는 이벤트가 발생하였는가?
-             running = False # 게임이 진행중이 아님
+    def wooden_sword(self):
+        print("나무 검")
+    def stone_sword(self):
+        print("돌 검")  
+    def iron_sword(self):
+        print("철 검")
+    def golden_sword(self):
+        print("금 검")
+    def diamond_sword(self):
+        print("다이아몬드 검")
+    def netherite_sword(self):
+        print("네더라이트 검")
+sword = Sword(10,20)
+sword.iron_sword()
+# pygame.init()
+# root.geometry()
 
-pygame.quit()
+
+# screen_width = 480 # 가로 크기
+# screen_height = 640 # 세로 크기
+# pygame.display.set_mode((screen_width, screen_height))
+
+# running = True
+# while running:
+#     for event in pygame.event.get(): # 어떤 이벤트가 발생하였는가?
+#          if event.type == pygame.QUIT: # 창이 닫히는 이벤트가 발생하였는가?
+#              running = False # 게임이 진행중이 아님
+
+# pygame.quit()
