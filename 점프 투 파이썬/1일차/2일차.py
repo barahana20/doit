8662c57df0f980ba5156989ec96e75a58d16dff2
Mr_Lee <49835246+barahana20@users.noreply.github.com>
2021-01-24 22:57:54 +0900

    enter

---
+++ 점프 투 파이썬/1일차/2일차.py
@@ -0,0 +1,15 @@
+print("life" "is" "too short")
+print("life"+"is"+"too short")
+print("life","is","too short")
+
+s1 = set([1,2,3])
+s2 = set([4,5,6])
+
+print(s1&s2)
+print(s1|s2)
+
+s1.add(4)
+print(s1)
+
+s1.update([4,5,6])
+print(s1)
