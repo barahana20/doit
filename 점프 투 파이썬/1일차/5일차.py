da5cdd5720c60b6b6ed72313de6410c381dd71d1
Mr_Lee <49835246+barahana20@users.noreply.github.com>
2021-01-31 18:50:57 +0900

    r

--- 점프 투 파이썬/1일차/5일차.py
+++ 점프 투 파이썬/1일차/5일차.py
@@ -1,4 +1,14 @@
 import re
+p = re.compile("^python\s\w+")
+
+data = """python one
+life is too short
+python two
+you need python 
+python three"""
+
+print(p.findall(data))
+
 p = re.compile("^python\s\w+", re.MULTILINE)
 
 data = """python one
@@ -8,3 +18,4 @@ you need python
 python three"""
 
 print(p.findall(data))
+
