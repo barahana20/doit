c4a5dbc07a2365a4f8bf537b54de4455efb36d29
Mr_Lee <49835246+barahana20@users.noreply.github.com>
2021-02-03 23:26:05 +0900

    .

--- 점프 투 파이썬/1일차/5일차.py
+++ 점프 투 파이썬/1일차/5일차.py
@@ -2,10 +2,10 @@ import re
 p = re.compile("^python\s\w+")
 
 data = """python one
-life is too short
+life is too short 
 python two
 you need python
-python three""" 
+python three"""
 
 print(p.findall(data))
 
